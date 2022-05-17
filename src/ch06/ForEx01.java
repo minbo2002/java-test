e67429de1c66e5c524ed6665ab492dcadcb89ee0
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-05-17 23:50:13 +0900

    for문 Object

---
+++ src/ch06/ForEx01.java
@@ -0,0 +1,17 @@
+package ch06;
+
+public class ForEx01 {
+
+    public static void main(String[] args) {
+
+        int score=95;
+
+        if(score > 90) {
+            System.out.println("A");
+        }else if(score > 80){
+            System.out.println("B");
+        }else {
+            System.out.println("C");
+        }
+    }
+}
